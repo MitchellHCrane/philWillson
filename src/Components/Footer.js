51f9b7ab76fe969e493b8684e3579f727c65eb89
Mitchell Crane <mitch@symatri.com>
2022-12-15 09:53:28 -0700

    upload files

---
+++ src/Components/Footer.js
@@ -0,0 +1,47 @@
+import "../css/footer.css";
+import "../css/App.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-div">
+        <div className="footerGridItem">
+          <h3 className="footerH3">Contact</h3>
+          <p className="footerP">
+            Phone: <a href="tel:801-209-4744">(801)-209-4744</a>
+          </p>
+          <p className="footerP">Fax: (801)-951-5213</p>
+          <p className="footerP">
+            Email:{" "}
+            <a href="mailto:linda@firstclasshomemortgage.com">
+              Linda@firstclasshomemortgage.com
+            </a>
+          </p>
+        </div>
+        <div className="footerGridItem">
+          <h3 className="footerH3">License Info</h3>
+          <p className="footerP">Linda Skehan (NMLS #262918)</p>
+          <p className="footerP">
+            Regulated by the Colorado Divison of Real Estate NMLS #1843
+          </p>
+        </div>
+        <div className="footerGridItem">
+          <h3 className="footerH3">Address</h3>
+          <p className="footerP">First Class Home Mortgage</p>
+          <p className="footerP">
+            <a
+              href="https://goo.gl/maps/o97dcDxGxFcAcRheA"
+              target="_blank"
+              rel="noreferrer"
+            >
+              10808 River Front Parkway, Suite #3035,{<br />} South Jordan, UT
+              84095
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Components/Profile.js
@@ -0,0 +1,46 @@
+import "../css/profile.css";
+import "../css/App.css";
+import ParticlesBg from "particles-bg";
+
+function Profile() {
+  return (
+    <div className="profile-div">
+      <div className="particle-bg">
+        <ParticlesBg
+          color="#2ca4f2"
+          num={75}
+          type="cobweb"
+          bg={true}
+          config={{ rps: 0.00001 }}
+        />
+      </div>
+      <div className="profile-grid">
+        <div className="columnPic">
+          <img
+            className="troyProfilePic"
+            src="../images/lindaSkehan.jpg"
+            alt="Linda Skehan Mortgage Loan Officer
+
+            "
+          />
+        </div>
+        <div>
+          <h1 className="profileName">Linda Skehan</h1>
+          <h2 className="NMLS">Loan Officer (NMLS #262918)</h2>
+          <p className="profileP">
+            Hello! I’m Linda, I look forward to helping you along your home
+            buying experience. I’ve provided a few useful links to get you
+            started.
+          </p>
+          <div className="buttonBlueDiv">
+            <a href="#getStarted" className="buttonBlue">
+              Get Started
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
